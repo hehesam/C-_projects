264070efcdaba0d8f60304ccee6e1a21d6481676
Hesam Mohebi <mohebi.hesam@gmail.com>
2022-02-17 16:45:00 +0330

    first c# project is here baby

---
+++ C_Sharp_1/C_Sharp_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_Sharp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int word = Convert.ToInt32( Console.ReadLine());
+              
+            Console.WriteLine(word);  
+        }
+    }
+}
